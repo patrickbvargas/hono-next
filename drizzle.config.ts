d91fa20909839385310cbc092837b4f64868c683
Patrick Vargas <patrick.barcellos@hotmail.com.br>
2025-06-14 15:57:53 -0300

    First commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  out: "./src/server/db/migrations",
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+} satisfies Config;
