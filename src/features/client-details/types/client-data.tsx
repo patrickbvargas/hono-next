426d3adfbefaeccfbe6b4756cb3241efde848cf9
Patrick Vargas <patrick.barcellos@hotmail.com.br>
2025-06-29 20:28:32 +0000

    feat: add client details page

---
+++ src/features/client-details/types/client-data.tsx
@@ -0,0 +1,7 @@
+import type { DefinitionItemData } from "~/shared/types/definition-data";
+
+export type ClientData = {
+  identification: DefinitionItemData[];
+  contact: DefinitionItemData[];
+  details: DefinitionItemData[];
+};
